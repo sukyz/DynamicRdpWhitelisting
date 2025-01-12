f986f1e0ebaa464b95641732b5459049c6cf295a
sukyz <13932316+sukyz@users.noreply.github.com>
2025-01-12 19:07:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -31,3 +31,33 @@ func main() {
 					</form>
 				</body>
 				</html>
+		`
+		w.Write([]byte(htmlForm))
+	case "POST":
+		r.ParseForm()
+		clientPassword := r.FormValue("password")
+		clientIP := r.RemoteAddr
+
+		if clientPassword == password {
+			// 添加防火墙规则
+			cmd := exec.Command("netsh", "advfirewall", "firewall", "add", "rule", "name=\"Allow RDP\"", "dir=in", "action=allow", "protocol=TCP", "localport=3389", "remoteip="+clientIP)
+			if err := cmd.Run(); err != nil {
+				http.Error(w, "Failed to update firewall rules", http.StatusInternalServerError)
+				return
+			}
+
+			// 设置定时器，5小时后删除规则
+			time.AfterFunc(5*time.Hour, func() {
+				exec.Command("netsh", "advfirewall", "firewall", "delete", "rule", "name=\"Allow RDP\"", "remoteip="+clientIP).Run()
+			})
+
+			w.Write([]byte("IP added to whitelist successfully"))
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+})
+
+http.ListenAndServe(":8080", nil)
