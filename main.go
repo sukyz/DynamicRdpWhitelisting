26c902cb5095b69951b58d470997bee30dfc7986
sukyz <13932316+sukyz@users.noreply.github.com>
2025-01-12 19:05:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,32 +12,22 @@ const (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			r.ParseForm()
-			clientPassword := r.FormValue("password")
-			clientIP := r.RemoteAddr
-
-			if clientPassword == password {
-				// 添加防火墙规则
-				cmd := exec.Command("netsh", "advfirewall", "firewall", "add", "rule", "name=\"Allow RDP\"", "dir=in", "action=allow", "protocol=TCP", "localport=3389", "remoteip="+clientIP)
-				if err := cmd.Run(); err != nil {
-					http.Error(w, "Failed to update firewall rules", http.StatusInternalServerError)
-					return
-				}
-
-				// 设置定时器，5小时后删除规则
-				time.AfterFunc(5*time.Hour, func() {
-					exec.Command("netsh", "advfirewall", "firewall", "delete", "rule", "name=\"Allow RDP\"", "remoteip="+clientIP).Run()
-				})
-
-				w.Write([]byte("IP added to whitelist successfully"))
-			} else {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			}
-		} else {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-		}
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
+		switch r.Method {
+		case "GET":
+			// 提供一个HTML表单
+			htmlForm := `
+				
+```html
+<!DOCTYPE html>
+				<html>
+				<head>
+					<title>Enter Password to Whitelist IP</title>
+				</head>
+				<body>
+					<form action="/" method="post">
+						<label for="password">Password:</label>
+						<input type="password" id="password" name="password"><br><br>
+						<input type="submit" value="Submit">
+					</form>
+				</body>
+				</html>
